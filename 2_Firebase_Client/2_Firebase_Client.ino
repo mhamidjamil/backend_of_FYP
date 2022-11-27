2e7f3a234f2cd167a5b5ce72af63ff47ca5126b1
M Hamid Jamil <m.hamid.jamil99@gmail.com>
2022-11-27 23:05:07 +0500

    fireStore_connected

--- 2_Firebase_Client/2_Firebase_Client.ino
+++ 2_Firebase_Client/2_Firebase_Client.ino
@@ -1,8 +1,5 @@
-// forked from ThatProject
 
-// #include "DHTesp.h" // Click here to get the library: http://librarymanager/All#DHTesp
 #include <Ticker.h>
-// #include "Display.h"
 #include "Network.h"
 
 #ifndef ESP32
@@ -10,16 +7,8 @@
 #error Select ESP32 board.
 #endif
 
-/**************************************************************/
-/* Example how to read DHT sensors from an ESP32 using multi- */
-/* tasking.                                                   */
-/* This example depends on the Ticker library to wake up      */
-/* the task every 20 seconds                                  */
-/**************************************************************/
-
 // Display *display;
 Network *network;
-// DHTesp dht;
 
 void tempTask(void *pvParameters);
 bool getTemperature();
@@ -29,29 +18,12 @@ void triggerGetTemp();
 TaskHandle_t tempTaskHandle = NULL;
 /** Ticker for temperature reading */
 Ticker tempTicker;
-/** Comfort profile */
-ComfortState cf;
 /** Flag if task should run */
 bool tasksEnabled = false;
-/** Pin number for DHT11 data pin */
-int dhtPin = 21;
 
-/**
- * initTemp
- * Setup DHT library
- * Setup task and timer for repeated measurement
- * @return bool
- *    true if task and timer are started
- *    false if task or timer couldn't be started
- */
 bool initTemp()
 {
   byte resultValue = 0;
-  // Initialize temperature sensor
-  //~ dht.setup(dhtPin, DHTesp::DHT11);
-  // Serial.println("DHT initiated");
-
-  // Start task to get temperature
   xTaskCreatePinnedToCore(
       tempTask,        /* Function to implement the task */
       "tempTask ",     /* Name of the task */
@@ -74,11 +46,6 @@ bool initTemp()
   return true;
 }
 
-/**
- * triggerGetTemp
- * Sets flag dhtUpdated to true for handling in loop()
- * called by Ticker getTempTimer
- */
 void triggerGetTemp()
 {
   if (tempTaskHandle != NULL)
@@ -87,11 +54,6 @@ void triggerGetTemp()
   }
 }
 
-/**
- * Task to reads temperature from DHT11 sensor
- * @param pvParameters
- *    pointer to task parameters
- */
 void tempTask(void *pvParameters)
 {
   Serial.println("tempTask loop started");
@@ -99,79 +61,17 @@ void tempTask(void *pvParameters)
   {
     if (tasksEnabled)
     {
-      // Get temperature values
       getTemperature();
     }
     // Got sleep again
     vTaskSuspend(NULL);
   }
 }
-
-/**
- * getTemperature
- * Reads temperature from DHT11 sensor
- * @return bool
- *    true if temperature could be aquired
- *    false if aquisition failed
- */
 bool getTemperature()
 {
-  // Reading temperature for humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
-  TempAndHumidity newValues = dht.getTempAndHumidity();
-  // Check if any reads failed and exit early (to try again).
-  if (dht.getStatus() != 0)
-  {
-    Serial.println("DHT11 error status: " + String(dht.getStatusString()));
-    return false;
-  }
 
-  float heatIndex = dht.computeHeatIndex(newValues.temperature, newValues.humidity);
-  float dewPoint = dht.computeDewPoint(newValues.temperature, newValues.humidity);
-  float cr = dht.getComfortRatio(cf, newValues.temperature, newValues.humidity);
-
-  String comfortStatus;
-  switch (cf)
-  {
-  case Comfort_OK:
-    comfortStatus = "OK";
-    break;
-  case Comfort_TooHot:
-    comfortStatus = "TooHot";
-    break;
-  case Comfort_TooCold:
-    comfortStatus = "TooCold";
-    break;
-  case Comfort_TooDry:
-    comfortStatus = "TooDry";
-    break;
-  case Comfort_TooHumid:
-    comfortStatus = "TooHumid";
-    break;
-  case Comfort_HotAndHumid:
-    comfortStatus = "Hot&Humid";
-    break;
-  case Comfort_HotAndDry:
-    comfortStatus = "Hot&Dry";
-    break;
-  case Comfort_ColdAndHumid:
-    comfortStatus = "Cold&Humid";
-    break;
-  case Comfort_ColdAndDry:
-    comfortStatus = "Cold&Dry";
-    break;
-  default:
-    comfortStatus = "Unknown";
-    break;
-  };
-
-  Serial.println(" T:" + String(newValues.temperature) + " H:" + String(newValues.humidity) + " I:" + String(heatIndex) + " D:" + String(dewPoint) + " " + comfortStatus);
-
-  display->tempUpdates("Temp " + String(newValues.temperature, 1) + "'C",
-                       "Humidity " + String(newValues.humidity, 0) + "%",
-                       comfortStatus);
-
-  network->firestoreDataUpdate(newValues.temperature, newValues.humidity);
+  //    Serial.println("DHT11 error status: " + String(dht.getStatusString()));
+  network->firestoreDataUpdate(random(35, 40), random(45, 50));
 
   return true;
 }
@@ -182,7 +82,7 @@ void setup()
   Serial.println();
   Serial.println("DHT ESP32 example with tasks");
 
-  initDisplay();
+  //  initDisplay();
   initNetwork();
   initTemp();
   // Signal end of setup() to tasks
@@ -203,13 +103,6 @@ void loop()
     }
   }
   yield();
-}
-
-void initDisplay()
-{
-  display = new Display();
-  display->initTFT();
-  display->centerMsg("System Init...");
 }
 
 void initNetwork()
