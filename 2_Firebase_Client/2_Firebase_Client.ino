7ad8bf67fcb071e25b2fa9335ee9824c1f332a38
M Hamid Jamil <m.hamid.jamil99@gmail.com>
2022-11-30 01:44:38 +0500

    Serial_managment_done

--- 2_Firebase_Client/2_Firebase_Client.ino
+++ 2_Firebase_Client/2_Firebase_Client.ino
@@ -152,17 +152,7 @@ void initNetwork()
 
 void DataManager(String tempstr)
 {
-  // perform operation on given string, and assign to different variables
-  // b'25.18!Supine position!C:0.00,R:0.00,V:0.0000!0.00!'
-  // 25.18 temperature here and will be stored in filterd_temperature variable
-  // Supine position will be stored in position variable
-  // C:0.00,R:0.00,V:0.0000 will be stored in filterd_Conductance, filterd_Resistance, filterd_Conductance_voltage
-  // 0.00 will be stored in filterd_snore_voltages
-  // ? data to be managed in struct
-  // b'25.18!Supine position!C:0.00,R:0.00,V:0.0000!0.00!'
-  // int data_input=0;
-
-  int filterd_temperatur = 0;
+  float filterd_temperatur = 0;
 
   String position = "";
 
@@ -170,41 +160,43 @@ void DataManager(String tempstr)
   float filterd_Resistance = 0.0;
   float filterd_Conductance_voltage = 0.0;
 
-  float filterd_snore_voltages=0.0;
-
-  // ? data manager variables end
+  float filterd_snore_voltages = 0.0;
   int first_exclamation = tempstr.indexOf('!');
   int second_exclamation = tempstr.indexOf('!', first_exclamation + 1);
   int third_exclamation = tempstr.indexOf('!', second_exclamation + 1);
+  int fourth_exclamation = tempstr.indexOf('!', third_exclamation + 1);
+
+  // Serial.println("1st ! sppoted at position : " + String(first_exclamation));
+  // Serial.println("2nd ! sppoted at position : " + String(second_exclamation));
+  // Serial.println("3rd ! sppoted at position : " + String(third_exclamation));
 
   String temp = tempstr.substring(2, first_exclamation);
-  filterd_temperatur = temp.toInt();
+  filterd_temperatur = temp.toFloat();
+
   Serial.println("filterd_temperatur: " + String(filterd_temperatur));
 
   position = tempstr.substring(first_exclamation + 1, second_exclamation);
   Serial.println("position: " + position);
 
   String temp1 = tempstr.substring(second_exclamation + 1, third_exclamation);
-  int first_colon = temp1.indexOf(':');
-  int second_colon = temp1.indexOf(':', first_colon + 1);
-  int third_colon = temp1.indexOf(':', second_colon + 1);
+  int first_coma = temp1.indexOf(',');
+  int second_coma = temp1.indexOf(',', first_coma + 1);
+  int third_coma = temp1.indexOf(',', second_coma + 1);
 
-  String temp2 = temp1.substring(0, first_colon);
+  String temp2 = temp1.substring(0, first_coma);
   filterd_Conductance = temp2.toFloat();
   Serial.println("filterd_Conductance: " + String(filterd_Conductance));
 
-  String temp3 = temp1.substring(first_colon + 1, second_colon);
+  String temp3 = temp1.substring(first_coma + 1, second_coma);
   filterd_Resistance = temp3.toFloat();
   Serial.println("filterd_Resistance: " + String(filterd_Resistance));
 
-  String temp4 = temp1.substring(second_colon + 1, third_colon);
+  String temp4 = temp1.substring(second_coma + 1, third_exclamation);
   filterd_Conductance_voltage = temp4.toFloat();
-  Serial.println("filterd_Conductance_voltage: " + String(filterd_Conductance_voltage));
+  Serial.println("filterd_Conductance_voltage: " +
+                 String(filterd_Conductance_voltage));
 
-  String temp5 = tempstr.substring(third_exclamation + 1);
+  String temp5 = tempstr.substring(third_exclamation + 1, fourth_exclamation);
   filterd_snore_voltages = temp5.toFloat();
-  // Serial.println("filterd_snore_voltages: " + String(filterd_snore_v
-
-
-
+  Serial.println("filterd_snore_voltages: " + String(filterd_snore_voltages));
 }
